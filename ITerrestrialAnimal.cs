5f0ef4cd5b7d22aba33cc70f5064ff669b97b9ba
ronngo <ronngomn23@gmail.com>
2020-09-23 21:18:47 +0700

    first commit

---
+++ ITerrestrialAnimal.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace animalmanagementsystem
+{
+    public interface ITerrestrialAnimal : IAnimal
+    {
+        public void Move();
+    }
+}
